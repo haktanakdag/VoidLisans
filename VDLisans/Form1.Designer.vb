5ab476603d5d8298eb4ad863dd3e61f5bd522e73
Haktan <haktan.akdag@gmail.com>
2018-09-18 10:32:01 +0300

    Add files via upload

---
+++ VDLisans/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtSeriNo = New System.Windows.Forms.TextBox()
+        Me.txtLisansNo = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnYapistir = New System.Windows.Forms.Button()
+        Me.btnKopyala = New System.Windows.Forms.Button()
+        Me.btnLisansOlustur = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtSeriNo
+        '
+        Me.txtSeriNo.Location = New System.Drawing.Point(94, 21)
+        Me.txtSeriNo.Name = "txtSeriNo"
+        Me.txtSeriNo.Size = New System.Drawing.Size(1036, 22)
+        Me.txtSeriNo.TabIndex = 7
+        '
+        'txtLisansNo
+        '
+        Me.txtLisansNo.Location = New System.Drawing.Point(97, 61)
+        Me.txtLisansNo.Name = "txtLisansNo"
+        Me.txtLisansNo.Size = New System.Drawing.Size(1033, 22)
+        Me.txtLisansNo.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 17)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Lisans No:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 17)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Seri No:"
+        '
+        'btnYapistir
+        '
+        Me.btnYapistir.Location = New System.Drawing.Point(1136, 12)
+        Me.btnYapistir.Name = "btnYapistir"
+        Me.btnYapistir.Size = New System.Drawing.Size(122, 38)
+        Me.btnYapistir.TabIndex = 9
+        Me.btnYapistir.Text = "Yapıştır"
+        Me.btnYapistir.UseVisualStyleBackColor = True
+        '
+        'btnKopyala
+        '
+        Me.btnKopyala.Location = New System.Drawing.Point(1136, 56)
+        Me.btnKopyala.Name = "btnKopyala"
+        Me.btnKopyala.Size = New System.Drawing.Size(122, 38)
+        Me.btnKopyala.TabIndex = 8
+        Me.btnKopyala.Text = "Kopyala"
+        Me.btnKopyala.UseVisualStyleBackColor = True
+        '
+        'btnLisansOlustur
+        '
+        Me.btnLisansOlustur.Location = New System.Drawing.Point(1136, 100)
+        Me.btnLisansOlustur.Name = "btnLisansOlustur"
+        Me.btnLisansOlustur.Size = New System.Drawing.Size(122, 53)
+        Me.btnLisansOlustur.TabIndex = 10
+        Me.btnLisansOlustur.Text = "Lisans Oluştur"
+        Me.btnLisansOlustur.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1270, 172)
+        Me.Controls.Add(Me.btnLisansOlustur)
+        Me.Controls.Add(Me.btnYapistir)
+        Me.Controls.Add(Me.btnKopyala)
+        Me.Controls.Add(Me.txtSeriNo)
+        Me.Controls.Add(Me.txtLisansNo)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "VOID Lisans"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtSeriNo As TextBox
+    Friend WithEvents txtLisansNo As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnYapistir As Button
+    Friend WithEvents btnKopyala As Button
+    Friend WithEvents btnLisansOlustur As Button
+End Class
---
+++ VDLisans/Form1.vb
@@ -0,0 +1,96 @@
+﻿Public Class Form1
+    Private Sub btnLisansOlustur_Click(sender As Object, e As EventArgs) Handles btnLisansOlustur.Click
+        If txtSeriNo.Text = "" Then
+            MessageBox.Show("Seri No Boş olamaz")
+        Else
+            Dim serino As String = Decrypt(txtSeriNo.Text, "4524423423523423")
+            'MessageBox.Show(serino)
+            Dim yeniserino As String = Encrypt(serino, "344342132234543")
+            txtLisansNo.Text = yeniserino
+
+        End If
+    End Sub
+    Function SayilariCikart(lisansseri As String) As String
+        Dim lisansarr As Array = lisansseri.ToCharArray()
+        Dim karakterler As Integer = 0
+        For Each r As Char In lisansarr
+            If IsNumeric(r) = True Then
+                karakterler = karakterler - Convert.ToInt32(r)
+            End If
+        Next r
+        Return karakterler.ToString
+    End Function
+    Function SayilariTopla(lisansseri As String) As String
+        Dim lisansarr As Array = lisansseri.ToCharArray()
+        Dim karakterler As Integer = 0
+        For Each r As Char In lisansarr
+            If IsNumeric(r) = True Then
+                karakterler = karakterler + Convert.ToInt32(r)
+            End If
+        Next r
+        Return karakterler.ToString
+    End Function
+    Function SayilariBirlestir(lisansseri As String) As String
+        Dim lisansarr As Array = lisansseri.ToCharArray()
+        Dim sayilar As String = ""
+        For Each r As Char In lisansarr
+            If IsNumeric(r) = True Then
+                sayilar = sayilar & r.ToString
+            End If
+        Next r
+
+        Return sayilar.ToString
+    End Function
+
+    Public Function Encrypt(text As String, password As String) As String
+        Dim AES As New System.Security.Cryptography.RijndaelManaged
+        Dim Hash_AES As New System.Security.Cryptography.MD5CryptoServiceProvider
+        Dim encrypted As String = ""
+        Dim hash(31) As Byte
+        Dim temp As Byte() = Hash_AES.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(password))
+        Array.Copy(temp, 0, hash, 0, 16)
+        Array.Copy(temp, 0, hash, 15, 16)
+        AES.Key = hash
+        AES.Mode = Security.Cryptography.CipherMode.ECB
+        Dim DESEncrypter As System.Security.Cryptography.ICryptoTransform = AES.CreateEncryptor
+        Dim Buffer As Byte() = System.Text.ASCIIEncoding.ASCII.GetBytes(text)
+        encrypted = Convert.ToBase64String(DESEncrypter.TransformFinalBlock(Buffer, 0, Buffer.Length))
+        Return encrypted
+    End Function
+    Public Function Decrypt(text As String, password As String) As String
+        Dim AES As New System.Security.Cryptography.RijndaelManaged
+        Dim Hash_AES As New System.Security.Cryptography.MD5CryptoServiceProvider
+        Dim decrypted As String = ""
+        Dim hash(31) As Byte
+        Dim temp As Byte() = Hash_AES.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(password))
+        Array.Copy(temp, 0, hash, 0, 16)
+        Array.Copy(temp, 0, hash, 15, 16)
+        AES.Key = hash
+        AES.Mode = Security.Cryptography.CipherMode.ECB
+        Dim DESDecrypter As System.Security.Cryptography.ICryptoTransform = AES.CreateDecryptor
+        Dim Buffer As Byte() = Convert.FromBase64String(text)
+        Dim lisansvar As Boolean = False
+        Try
+            decrypted = System.Text.ASCIIEncoding.ASCII.GetString(DESDecrypter.TransformFinalBlock(Buffer, 0, Buffer.Length))
+            lisansvar = True
+        Catch ex As Exception
+            lisansvar = False
+        End Try
+        If lisansvar = False Then
+            decrypted = "lisansyok"
+        End If
+        Return decrypted
+    End Function
+
+    Private Sub btnYapistir_Click(sender As Object, e As EventArgs) Handles btnYapistir.Click
+        txtSeriNo.Text = Clipboard.GetText
+    End Sub
+
+    Private Sub btnKopyala_Click(sender As Object, e As EventArgs) Handles btnKopyala.Click
+        If txtLisansNo.Text = "" Then
+            MessageBox.Show("Lisans Numarası boş lütfen lisans oluşturunuz.")
+        Else
+            Clipboard.SetText(txtLisansNo.Text)
+        End If
+    End Sub
+End Class
